f1b463453f9a5a941ccec11e30e76b0ffcbe6b07
ANDANTINO <AHT_666@mail.ru>
2024-01-16 20:53:12 +0500

    Added link click metrics

---
+++ docs/assets/js/metrics.js
@@ -0,0 +1,21 @@
+if (typeof SiteMetrics !== typeof undefined && SiteMetrics.baseUrl) {
+    window.addEventListener("click", async function (args) {
+        const meterName = args.target.dataset.meterName;
+
+        if (meterName && args.target.tagName === "A") {
+            try {
+                _ = await fetch(`${SiteMetrics.baseUrl}/githubpages/metrics`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json;charset=utf-8"
+                    },
+                    body: JSON.stringify({ "MeterName": meterName })
+                });
+            } catch (error) {
+                if (SiteMetrics.showError) {
+                    console.debug(`Fetch error: ${error.message}`);
+                }
+            }
+        }
+    }, false);
+}
